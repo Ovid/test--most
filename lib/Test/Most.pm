3a8ce9a8da7c624196c36e98504f7473b9820de9
Chad Granum <chad.granum@dreamhost.com>
2014-06-26 10:32:29 -0700

    Add support for Result_Stream branch of Test::Builder
    This is the only thing that NEEDS to change to make Test::Most work with
    the new Test::Builder/Test::More changes in Result_Stream.
    
    Ultimately some other chnages may be a good idea/helpful, but because of
    backwards compatability support they are not strictly necessary.

--- lib/Test/Most.pm
+++ lib/Test/Most.pm
@@ -12,11 +12,17 @@ my $HAVE_TIME_HIRES;
 
 BEGIN {
 
-    # There's some strange fiddling around with import(), so this allows us to
-    # be nicely backwards compatible to earlier versions of Test::More.
     require Test::More;
-    @Test::More::EXPORT = grep { $_ ne 'explain' } @Test::More::EXPORT;
-    Test::More->import;
+    if (Test::More->can('TB_PROVIDER_META')) {
+        Test::More->import(import => [ '!explain' ]);
+    }
+    else {
+        # There's some strange fiddling around with import(), so this allows us to
+        # be nicely backwards compatible to earlier versions of Test::More.
+        @Test::More::EXPORT = grep { $_ ne 'explain' } @Test::More::EXPORT;
+        Test::More->import;
+    }
+
     eval "use Time::HiRes";
     $HAVE_TIME_HIRES = 1 unless $@;
 }
@@ -439,7 +445,9 @@ it's a great and useful module.
 BEGIN {
     @ISA    = qw(Test::Builder::Module);
     @EXPORT = (
-        @Test::More::EXPORT, 
+        Test::More->can('TB_PROVIDER_META')
+            ? keys( %{Test::More->TB_PROVIDER_META->{attrs}})
+            : @Test::More::EXPORT,
         qw<
             all_done
             bail_on_fail
