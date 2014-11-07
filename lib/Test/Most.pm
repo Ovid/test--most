fee93de0b26f37d0376af339ffa841c8f4ae7170
Karen Etheridge <ether@cpan.org>
2014-11-07 15:05:36 -0800

    ensure Test::Builder's original DESTROY is still called
    This is *very important* for cleaning up after exceptions that occurred within
    subtests.

--- lib/Test/Most.pm
+++ lib/Test/Most.pm
@@ -278,15 +278,17 @@ sub set_failure_handler {
 
 {
     no warnings 'redefine';
+    my $orig_destroy = Test::Builder->can('DESTROY');
 
     # we need this because if the failure is on the final test, we won't have
     # a subsequent test triggering the behavior.
-    sub Test::Builder::DESTROY {
+    *Test::Builder::DESTROY = sub {
         my $builder = $_[0];
         if ( $builder->{TEST_MOST_test_failed} ) {
             ( $builder->{TEST_MOST_failure_action} || sub {} )->();
         }
-    }
+        $orig_destroy->(@_);
+    };
 }
 
 sub _deferred_plan_handler {
